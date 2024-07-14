b88b3d2fe8a4abb112dec82ae4ffea3b243c39b2
Sebastian Cardona <cardonaloaizasebastian112@gmail.com>
2024-07-14 06:13:25 -0500

    obtener datos de usuario

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from middlewares.error_handler import ErrorHandler
 
 from routers.fund_router import fund_router
 from routers.transaction_router import transaction_router
+from routers.user_router import user_router
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
@@ -22,6 +23,7 @@ app.add_middleware(ErrorHandler)
 
 app.include_router(fund_router)
 app.include_router(transaction_router)
+app.include_router(user_router)
 
 origins = [
     "http://localhost:3000",
---
+++ routers/user_router.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Path  # type: ignore
+from fastapi.responses import JSONResponse  # type: ignore
+from fastapi.encoders import jsonable_encoder  # type: ignore
+from typing import List
+from services.user_service import UserService
+from schemas.user_schema import UserSchema
+
+user_router = APIRouter()
+
+
+@user_router.get("/users/{user_id}", tags=["users"], response_model=UserSchema)
+def get_user(user_id: int = Path(ge=1, le=2000)) -> UserSchema:
+    userService = UserService()
+    result = userService.get_user(user_id)
+    if not result:
+        return JSONResponse(status_code=404, content={"message": "No encontrado"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
