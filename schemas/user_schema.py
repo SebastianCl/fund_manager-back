70ace24c126c66ee6ce6f508c4845fb33e25211b
Sebastian Cardona <cardonaloaizasebastian112@gmail.com>
2024-07-14 23:55:25 -0500

    campos email y numero en schema de usuario

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -5,6 +5,8 @@ class UserSchema(BaseModel):
     user_id: int
     name: str = Field(min_length=1, max_length=50)
     amount: int
+    email: str
+    phone: str
 
     class Config:
         schema_extra = {
@@ -12,5 +14,7 @@ class UserSchema(BaseModel):
                 "user_id": 1,
                 "name": "Pepita",
                 "amount": 75000,
+                "email": "uncorreo@dominio.com",
+                "phone": "+14343443",
             }
         }
